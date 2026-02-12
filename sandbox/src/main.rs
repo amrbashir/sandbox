879e33f1eced722aa17b05373ed60e1378d1d5c7
amrbashir <github@amrbashir.me>
2026-02-12 15:21:25 +0200

    feat: implement get_main_thread_handle function for stable rust thread access

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,15 +1,50 @@
-#![feature(windows_process_extensions_main_thread_handle)]
-
 use anyhow::Result;
 use clap::Parser;
 use std::os::windows::io::AsRawHandle;
-use std::os::windows::process::{ChildExt, CommandExt};
+use std::os::windows::process::CommandExt;
 use std::path::PathBuf;
 use std::process::Command;
+use std::sync::LazyLock;
 use windows::Win32::Foundation::*;
 use windows::Win32::System::JobObjects::*;
-use windows::Win32::System::LibraryLoader::GetModuleHandleW;
+use windows::Win32::System::LibraryLoader::{GetModuleHandleW, GetProcAddress};
 use windows::Win32::System::Threading::*;
+use windows::core::{s, w};
+
+#[allow(non_snake_case)]
+type NtGetNextThreadFn = unsafe extern "system" fn(
+    ProcessHandle: HANDLE,
+    ThreadHandle: HANDLE,
+    DesiredAccess: u32,
+    HandleAttributes: u32,
+    Flags: u32,
+    NewThreadHandle: *mut HANDLE,
+) -> NTSTATUS;
+
+#[allow(non_upper_case_globals)]
+static NtGetNextThread: LazyLock<Option<NtGetNextThreadFn>> = LazyLock::new(|| {
+    let ntdll = unsafe { GetModuleHandleW(w!("ntdll.dll")) }.ok()?;
+    let address = unsafe { GetProcAddress(ntdll, s!("NtGetNextThread")) }?;
+    unsafe { std::mem::transmute(address) }
+});
+
+/// Get's the handle of the main thread of a process in stable rust.
+pub unsafe fn get_main_thread_handle(
+    hprocess: HANDLE,
+    desired_access: u32,
+) -> windows::core::Result<HANDLE> {
+    if let Some(nt_get_next_thread) = *NtGetNextThread {
+        let mut hthread = HANDLE::default();
+        let status =
+            unsafe { nt_get_next_thread(hprocess, hthread, desired_access, 0, 0, &mut hthread) };
+
+        if status == STATUS_SUCCESS {
+            return Ok(hthread);
+        }
+    }
+
+    return Err(windows::core::Error::from_thread());
+}
 
 #[derive(Parser, Debug)]
 #[command(name = "sandbox")]
@@ -63,7 +98,7 @@ fn main() -> Result<()> {
         .spawn()?;
 
     let hprocess = HANDLE(child.as_raw_handle() as _);
-    let hthread = HANDLE(child.main_thread_handle().as_raw_handle() as _);
+    let hthread = unsafe { get_main_thread_handle(hprocess, THREAD_SUSPEND_RESUME.0)? };
 
     // Create shared memory with deny list before assigning to job
     if !args.deny.is_empty() {
